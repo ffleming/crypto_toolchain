773737803758a006a8df0db84702bd2c68c11bc9
Forrest Fleming <forrest.fleming@procore.com>
2017-08-16 14:11:30 -0700

    new key every run

--- lib/crypto_toolchain/black_boxes/ecb_prepend_chosen_plaintext_oracle.rb
+++ lib/crypto_toolchain/black_boxes/ecb_prepend_chosen_plaintext_oracle.rb
@@ -3,13 +3,17 @@ module CryptoToolchain
   module BlackBoxes
     class EcbPrependChosenPlaintextOracle
       MYSTERY_TEXT = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
-      KEY = "\xC9P\xAF\b\xC3=\"\x84\x9AS\xB12\xC9*\xB0\x18".freeze
+      def initialize(key: String.random_bytes(16))
+        @key = key
+      end
 
       def encrypt(plaintext)
-        obfuscate(plaintext).encrypt_ecb(key: KEY, blocksize: 16)
+        obfuscate(plaintext).encrypt_ecb(key: key, blocksize: 16)
       end
 
       private
+
+      attr_reader :key
 
       def obfuscate(str)
         "#{str}#{MYSTERY_TEXT.from_base64}"
