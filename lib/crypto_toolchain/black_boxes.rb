1e5541bf21eb4ed18a7d35781009efc0a987fb3e
Forrest Fleming <ffleming@gmail.com>
2017-08-27 09:56:50 -0700

    move requires to their own files

--- lib/crypto_toolchain/black_boxes.rb
+++ lib/crypto_toolchain/black_boxes.rb
@@ -1,3 +1,16 @@
+require "crypto_toolchain/black_boxes/aes_ctr_editor"
+require "crypto_toolchain/black_boxes/ecb_or_cbc_encryptor"
+require "crypto_toolchain/black_boxes/ecb_prepend_chosen_plaintext_oracle"
+require "crypto_toolchain/black_boxes/ecb_interpolate_chosen_plaintext_oracle"
+require "crypto_toolchain/black_boxes/ecb_cut_and_paste_target"
+require "crypto_toolchain/black_boxes/cbc_bitflip_target"
+require "crypto_toolchain/black_boxes/cbc_padding_oracle"
+require "crypto_toolchain/black_boxes/netcat_cbc_padding_oracle"
+require "crypto_toolchain/black_boxes/mt_19937"
+require "crypto_toolchain/black_boxes/mt_19937_stream_cipher"
+require "crypto_toolchain/black_boxes/aes_ctr_editor"
+require "crypto_toolchain/black_boxes/ctr_bitflip_target"
+
 module CryptoToolchain
   module BlackBoxes
 
--- lib/crypto_toolchain/tools.rb
+++ lib/crypto_toolchain/tools.rb
@@ -1,3 +1,14 @@
+require "crypto_toolchain/tools/determine_blocksize"
+require "crypto_toolchain/tools/ecb_prepend_chosen_plaintext_attack"
+require "crypto_toolchain/tools/ecb_interpolate_chosen_plaintext_attack"
+require "crypto_toolchain/tools/ecb_cut_and_paste_attack"
+require "crypto_toolchain/tools/cbc_bitflip_attack"
+require "crypto_toolchain/tools/cbc_padding_oracle_attack"
+require "crypto_toolchain/tools/interactive_xor"
+require "crypto_toolchain/tools/mt_19937_seed_recoverer"
+require "crypto_toolchain/tools/mt_19937_stream_cipher_seed_recoverer"
+require "crypto_toolchain/tools/aes_ctr_recoverer"
+
 module CryptoToolchain
   module Tools
     PRINTABLE_CHARS = ((0x20..0x7e).to_a + [0x0a, 0x0d]).map(&:chr).freeze
