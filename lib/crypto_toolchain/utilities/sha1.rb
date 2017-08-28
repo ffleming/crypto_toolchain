b5749da3440f0c47d022061e58f64b3aba479c66
Forrest Fleming <forrest.fleming@procore.com>
2017-08-28 14:08:22 -0700

    more useful padding method

--- lib/crypto_toolchain/utilities/sha1.rb
+++ lib/crypto_toolchain/utilities/sha1.rb
@@ -3,12 +3,12 @@ module CryptoToolchain
   module Utilities
     class SHA1
       class << self
-        def hexdigest(str)
-          CryptoToolchain::Utilities::SHA1.new(str).hexdigest
+        def hexdigest(str, registers: STARTING_REGISTERS)
+          CryptoToolchain::Utilities::SHA1.new(str).hexdigest(registers: registers)
         end
 
-        def bindigest(str)
-          CryptoToolchain::Utilities::SHA1.new(str).bindigest
+        def bindigest(str, registers: STARTING_REGISTERS)
+          CryptoToolchain::Utilities::SHA1.new(str).bindigest(registers: registers)
         end
 
         def registers_for(hex_str)
@@ -57,6 +57,19 @@ module CryptoToolchain
         h.pack("L>5")
       end
 
+      def padding
+        return @padding if defined? @padding
+        # We know that we're dealing with a bitlength that is a multiple of 8
+        # because we're working with chars.  To pad with a 1-bit, just go ahead
+        # and also pad with 7 0-bits.  That way we can just work with bytes
+        #
+        # The full message, including an 8-byte (64 bit) length segment, must
+        # be a multiple of 64 bytes (512 bits).  So pad such that
+        # str.length % 56 = 0.
+        num_null_pad = (56 - (original.bytesize + 1) ) % 64
+        @padding = 0x80.chr + (0.chr * num_null_pad) + [original.bytesize * 8].pack("Q>")
+      end
+
       private
 
       attr_reader :original
@@ -80,20 +93,7 @@ module CryptoToolchain
       end
 
       def preprocessed
-        @preprocessed ||= padded + [original.bytesize * 8].pack("Q>")
-      end
-
-      def padded
-        return @padded if defined? @padded
-        # We know that we're dealing with a bitlength that is a multiple of 8
-        # because we're working with chars.  To pad with a 1-bit, just go ahead
-        # and also pad with 7 0-bits.  That way we can just work with bytes
-        #
-        # The full message, including an 8-byte (64 bit) length segment, must
-        # be a multiple of 64 bytes (512 bits).  So pad such that
-        # str.length % 56 = 0.
-        num_null_pad = (56 - (original.bytesize + 1) ) % 64
-        @padded = "#{original}\x80#{0.chr * num_null_pad}"
+        @preprocessed ||= original + padding
       end
     end
   end
