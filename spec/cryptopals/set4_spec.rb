9bffd80ba8f9fcfc9987949d99bda3e84c4d4513
Forrest Fleming <ffleming@gmail.com>
2017-08-29 23:17:44 -0700

    specs

--- spec/cryptopals/set4_spec.rb
+++ spec/cryptopals/set4_spec.rb
@@ -26,4 +26,49 @@ RSpec.describe "Cryptopals Set 4" do
     mac = sha1.mac("Dogs are cool")
     expect(mac).to eq CryptoToolchain::Utilities::SHA1.hexdigest("woofDogs are cool")
   end
+
+  it "should perform a SHA1 length extension attack (29)" do
+    message = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+    add = ";admin=true"
+    validator = CryptoToolchain::BlackBoxes::SHA1Mac.new
+    mac = validator.mac(message)
+
+    found = false
+    (1..32).each do |key_len|
+      atk = CryptoToolchain::Tools::SHA1LengthExtensionAttack.new(message: message,
+                                                                  mac: mac,
+                                                                  add: add,
+                                                                  key_length: key_len)
+      forged_message, forged_mac = atk.execute
+      found ^= validator.valid?(message: forged_message, mac: forged_mac)
+    end
+    expect(found).to be true
+  end
+
+  it "should perform a MD4 length extension attack (30)" do
+    message = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+    add = ";admin=true"
+    validator = CryptoToolchain::BlackBoxes::MD4Mac.new
+    mac = validator.mac(message)
+
+    found = false
+    (1..32).each do |key_len|
+      atk = CryptoToolchain::Tools::MD4LengthExtensionAttack.new(message: message,
+                                                                  mac: mac,
+                                                                  add: add,
+                                                                  key_length: key_len)
+      forged_message, forged_mac = atk.execute
+      found ^= validator.valid?(message: forged_message, mac: forged_mac)
+    end
+    expect(found).to be true
+  end
+
+  it "should implement HMAC-SHA1 (30a)" do
+    10.times do |i|
+      key = Random.new.bytes(rand(1..128))
+      message = Random.new.bytes(rand(1..1024))
+      actual = CryptoToolchain::Utilities::HMAC.hexdigest(message, key: key, hash: CryptoToolchain::Utilities::SHA1)
+      expect(actual).to eq OpenSSL::HMAC.hexdigest('sha1', key, message)
+    end
+  end
 end
