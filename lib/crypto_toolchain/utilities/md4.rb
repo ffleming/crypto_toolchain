a4e5302cb0877ca14adb782f5efd96fc2cce5f07
Forrest Fleming <ffleming@gmail.com>
2017-08-30 14:06:47 -0700

    put other parameters in f,g,h functions

--- lib/crypto_toolchain/utilities/md4.rb
+++ lib/crypto_toolchain/utilities/md4.rb
@@ -41,22 +41,22 @@ module CryptoToolchain
 
           a, b, c, d = running_state
           [0, 4, 8, 12].each do |i|
-            a = (a + f(b, c, d) + x[i]).lrot(3)
-            d = (d + f(a, b, c) + x[i+1]).lrot(7)
-            c = (c + f(d, a, b) + x[i+2]).lrot(11)
-            b = (b + f(c, d, a) + x[i+3]).lrot(19)
+            a = f(a, b, c, d, x[i]).lrot(3)
+            d = f(d, a, b, c, x[i+1]).lrot(7)
+            c = f(c, d, a, b, x[i+2]).lrot(11)
+            b = f(b, c, d, a, x[i+3]).lrot(19)
           end
           [0, 1, 2, 3].each do |i|
-            a = (a + g(b, c, d) + x[i] + 0x5a827999).lrot(3)
-            d = (d + g(a, b, c) + x[i+4] + 0x5a827999).lrot(5)
-            c = (c + g(d, a, b) + x[i+8] + 0x5a827999).lrot(9)
-            b = (b + g(c, d, a) + x[i+12] + 0x5a827999).lrot(13)
+            a = g(a, b, c, d, x[i]).lrot(3)
+            d = g(d, a, b, c, x[i+4]).lrot(5)
+            c = g(c, d, a, b, x[i+8]).lrot(9)
+            b = g(b, c, d, a, x[i+12]).lrot(13)
           end
           [0, 2, 1, 3].each do |i|
-            a = (a + h(b, c, d) + x[i] + 0x6ed9eba1).lrot(3)
-            d = (d + h(a, b, c) + x[i+8] + 0x6ed9eba1).lrot(9)
-            c = (c + h(d, a, b) + x[i+4] + 0x6ed9eba1).lrot(11)
-            b = (b + h(c, d, a) + x[i+12] + 0x6ed9eba1).lrot(15)
+            a = h(a, b, c, d, x[i]).lrot(3)
+            d = h(d, a, b, c, x[i+8]).lrot(9)
+            c = h(c, d, a, b, x[i+4]).lrot(11)
+            b = h(b, c, d, a, x[i+12]).lrot(15)
           end
 
           [a, b, c, d].each_with_index do |val, i|
@@ -72,16 +72,24 @@ module CryptoToolchain
 
       attr_reader :original
 
-      def f(x, y, z)
-         (x & y) | (~x & z)
+      def f(arg, x, y, z, block)
+         arg +
+           ((x & y) | (~x & z)) +
+           block
       end
 
-      def g(x, y, z)
-        (x & y) | (x & z) | (y & z)
+      def g(arg, x, y, z, block)
+        arg +
+          ((x & y) | (x & z) | (y & z)) +
+          block +
+          0x5a827999
       end
 
-      def h(x, y, z)
-        x ^ y ^ z
+      def h(arg, x, y, z, block)
+        arg +
+          (x ^ y ^ z) +
+          block +
+          0x6ed9eba1
       end
 
       # Equivalent to [ 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 ]
