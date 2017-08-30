54a904e05ba61a233fdd9e693f5d96809ab106b3
Forrest Fleming <ffleming@gmail.com>
2017-08-30 11:32:31 -0700

    mild linting

--- lib/crypto_toolchain/utilities/md4.rb
+++ lib/crypto_toolchain/utilities/md4.rb
@@ -32,7 +32,7 @@ module CryptoToolchain
         f = proc {|x, y, z| x & y | x.^(mask) & z}
         g = proc {|x, y, z| x & y | x & z | y & z}
         h = proc {|x, y, z| x ^ y ^ z}
-        r = proc {|v, s| (v << s).&(mask) | (v.&(mask) >> (32 - s))}
+        r = proc {|v, s| v.lrot(s) }
 
         # initial hash
         a, b, c, d = registers_from(state)
@@ -47,26 +47,25 @@ module CryptoToolchain
         (message + padding).in_blocks(64).each do |block|
           x = block.unpack("L<16")
 
-          # Process this block.
           aa, bb, cc, dd = a, b, c, d
-          [0, 4, 8, 12].each {|i|
+          [0, 4, 8, 12].each do |i|
             a = r[a + f[b, c, d] + x[i],  3]; i += 1
             d = r[d + f[a, b, c] + x[i],  7]; i += 1
             c = r[c + f[d, a, b] + x[i], 11]; i += 1
             b = r[b + f[c, d, a] + x[i], 19]
-          }
-          [0, 1, 2, 3].each {|i|
+          end
+          [0, 1, 2, 3].each do |i|
             a = r[a + g[b, c, d] + x[i] + 0x5a827999,  3]; i += 4
             d = r[d + g[a, b, c] + x[i] + 0x5a827999,  5]; i += 4
             c = r[c + g[d, a, b] + x[i] + 0x5a827999,  9]; i += 4
             b = r[b + g[c, d, a] + x[i] + 0x5a827999, 13]
-          }
-          [0, 2, 1, 3].each {|i|
+          end
+          [0, 2, 1, 3].each do |i|
             a = r[a + h[b, c, d] + x[i] + 0x6ed9eba1,  3]; i += 8
             d = r[d + h[a, b, c] + x[i] + 0x6ed9eba1,  9]; i -= 4
             c = r[c + h[d, a, b] + x[i] + 0x6ed9eba1, 11]; i += 8
             b = r[b + h[c, d, a] + x[i] + 0x6ed9eba1, 15]
-          }
+          end
           a = (a + aa) & mask
           b = (b + bb) & mask
           c = (c + cc) & mask
