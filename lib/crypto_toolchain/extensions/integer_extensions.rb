24a6facaeefab00a139c29476297c8707655aeb6
Forrest Fleming <ffleming@gmail.com>
2017-08-31 20:20:03 -0700

    modpow

--- lib/crypto_toolchain/extensions/integer_extensions.rb
+++ lib/crypto_toolchain/extensions/integer_extensions.rb
@@ -13,4 +13,20 @@ class Integer
     ((self & 0xffffffff) >> num) |
       ((self << (32 - num)) & 0xffffffff)
   end
+
+  # Thanks Bruce Schneier
+  def modexp(exponent, mod)
+    raise ArgumentError.new("Exponent must be non-negative") if exponent < 0
+    product = 1
+    base = self % mod
+    until exponent == 0
+      if exponent & 0x01 == 1
+        product = (product * base) % mod
+      end
+      exponent = exponent >> 1
+      base = (base**2) % mod
+    end
+    product
+  end
+  alias_method :modpow, :modexp
 end
--- spec/lib/crypto_toolchain/extensions/integer_extensions_spec.rb
+++ spec/lib/crypto_toolchain/extensions/integer_extensions_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe "Integer extensions" do
       expect(0xb6fbbf7a.rrot(10)).to eq 0xdeadbeef
     end
   end
+
+  describe "#modexp" do
+    it "Should match OpenSSL's modexp output" do
+      10.times do
+        a = rand(1..1024)
+        e = rand(1..1024)
+        m = rand(1..1024)
+        expected = a.to_bn.mod_exp(e, m)
+        expect(a.modexp(e, m)).to eq expected
+      end
+    end
+  end
 end
